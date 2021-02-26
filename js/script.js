bf81770883213fa7d5cc311871a8f5b34b1deaaa
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-02-26 15:03:03 +0100

    add. js map

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+// 1 chiedere al clt km da percorrere
 
+// 2 chiedere età clt
+
+// 3 calcolare prezzo (0.21 euro al km)
+
+// 4 applicare sconto 20% minorenni 40% over 65
+
+// 5 esito costo su html
